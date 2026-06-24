0aa5b53bbfec3db1c9f39a2f1398d2f26526c403
egorkharaim <egor.kharaima@gmail.com>
2026-06-24 14:12:22 +0300

    Add Maven Spring Boot MySQL Liquibase project setup

---
+++ bankroll-web/src/test/java/com/bankrolldiscipline/manager/BankrollManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bankrolldiscipline.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankrollManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
